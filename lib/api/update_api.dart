aafffd1a5570956d34eeaaeed669cf80bfbe84ba
tiến <tiennguyen.wsm@gmail.com>
2026-09-14 01:32:03 +0700

    An muc Mac dinh trong am thanh va phu de, hien thi ro rang va dua lua chon len dau

--- lib/api/update_api.dart
+++ lib/api/update_api.dart
@@ -23,7 +23,7 @@ class UpdateApi {
   );
 
   // BẠN SẼ ĐỔI SỐ NÀY MỖI KHI RA MẮT BẢN CẬP NHẬT MỚI:
-  static const String currentAppVersion = '26.09.14.10.dev';
+  static const String currentAppVersion = '26.09.14.12.dev';
 
   /// Kiểm tra có bản cập nhật mới không
   static Future<UpdateInfo?> checkForUpdate() async {
